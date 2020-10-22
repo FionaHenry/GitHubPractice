869d1f1b38a44d43cb165ddfae50508e9d75c6b2
Fiona Henry <fionahenry87@googlemail.com>
2020-10-22 10:07:37 +0100

    Replaced adding methods with polymorphic versions

--- polymorphism/Program.cs
+++ polymorphism/Program.cs
@@ -6,12 +6,18 @@ namespace polymorphism
     {
         static void Main(string[] args)
         {
-            int addedSumOfInts = AddToInt(7, 13);
+            int addedSumOfInts = AddTwoNumbers(7, 13);
+            double addedSumOfDoubles = AddTwoNumbers(7.5, 13.8);
 
             Console.WriteLine("Int: " + addedSumOfInts);
+            Console.WriteLine("Double: " + addedSumOfDoubles);
         }
 
-        static int AddToInt(int x, int y)
+        static int AddTwoNumbers(int x, int y)
+        {
+            return x + y;
+        }
+        static double AddTwoNumbers(double x, double y)
         {
             return x + y;
         }
