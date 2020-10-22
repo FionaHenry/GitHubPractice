909d439b297003fdbd82874de79a89db73aba3bb
Fiona Henry <fionahenry87@googlemail.com>
2020-10-22 09:57:00 +0100

    dded int adding method

---
+++ polymorphism/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int addedSumOfInts = AddToInt(7, 13);
+
+            Console.WriteLine("Int: " + addedSumOfInts);
+        }
+
+        static int AddToInt(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
